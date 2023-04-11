2854caec038fac0f73492ff1402064c77948b4d4
halfalo <113610878+halfalo@users.noreply.github.com>
2023-04-11 18:49:29 +0200

    index.php html kód feltöltése

--- Php/index.php
+++ Php/index.php
@@ -15,4 +15,32 @@ $question = mysqli_fetch_assoc($qresult);
 $choices = $con->query("SELECT * FROM answers WHERE qid = $qid");
 
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>Kérdőív</title>
+    </head>
+    <body>
+    <div class="index">
+    <form method="POST" action="process.php" >
+        <h3 class="h2index"><label></label></h3>
+        <input type="hidden" name="question" value=" ">
+            <br>
+            <ul>
+                
+                <input type="radio" name ="answer" value=" ">
+                
+            <br>
+            </ul>
+            <br>
+
+            <input type="hidden" name="number" value=" ">
+            <button name="next" value="next">Tovább</button>
+            <br>
+        </form>
+    </div>
+    </body>
+</html>
 

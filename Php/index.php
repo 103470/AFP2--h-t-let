04af1dfc271ad5133d8730bb5c4f924108ee0c1a
Saviur <113610538+103470@users.noreply.github.com>
2023-04-11 17:54:55 +0200

    Index.php - kód megírása
    Kiszedi a szükséges adatokat az adatbázisból, majd megjeleníti azokat. Ezen az oldalon jelennek meg a kérdések

--- Php/index.php
+++ Php/index.php
@@ -1,3 +1,18 @@
 <?php  
 
+require 'connect.php'; ?>
+
+<?php
+
+$number = $_GET['n'];
+$qidresult = $con->query("SELECT qid FROM questions WHERE sn = $number");
+$qid = 1;
+while($row = mysqli_fetch_assoc($qidresult)){
+    $qid = $row['qid'];
+}
+$qresult = $con->query("SELECT * FROM questions WHERE sn = $number");
+$question = mysqli_fetch_assoc($qresult);
+$choices = $con->query("SELECT * FROM answers WHERE qid = $qid");
+
 ?>
+

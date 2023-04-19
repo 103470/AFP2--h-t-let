a8d5e2b2ebe9d299730675764b147e3b19c7f740
halfalo <113610878+halfalo@users.noreply.github.com>
2023-04-19 08:33:40 +0200

    login.php html kód elkészítése

--- Php/login.php
+++ Php/login.php
@@ -17,3 +17,38 @@
         }
     }
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Bejelentkezés</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+    <body>
+    <header>
+        <ul>
+            <li><a href="main.php">Kezdőlap</a></li>
+            <li><a href="login.php">Bejelentkezés</a></li>
+        </ul>
+    </header>
+        <div class="nagytest">
+            <h1 class="h1login">Bejelentkezés</h1>
+            <form action="login.php" method="POST">
+                <div class="test">
+                <label>Felhasználónév : </label>
+                <br>
+                <input type="text" name="username" placeholder="Írja be a felhasználónevet!" id="log" class="textbox" required>
+                <br><br>
+                <label>Jelszó : </label>
+                <br>
+                <input type="password" name="password" placeholder="Írja be a jelszót!" id="log" class="textbox" required>
+                <br><br>
+                <button type="submit" name="login" class="reglog">Bejelentkezés</button>
+                <br><br>
+                </div>
+            </form>
+        </div>
+    </body>
+
+</html>

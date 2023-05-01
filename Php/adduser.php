6677270d12cd0b2a8be0acad1e1e4ea59abf86fb
halfalo <113610878+halfalo@users.noreply.github.com>
2023-05-01 17:22:10 +0200

    html kód feltöltése az adduser.php-ba

--- Php/adduser.php
+++ Php/adduser.php
@@ -46,3 +46,53 @@ if(isset($_POST['adduser'])){
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Regisztráció</title>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <link rel="stylesheet" href="reglog.css" type="text/css">
+    </head>
+        <body>
+
+            <div class="containerreg">
+                <h1 class="h1reglog">Regisztráció</h1>
+                <form action="adduser.php" method="POST" enctype="multipart/form-data">
+                    <div class="test">
+                    <label>Felhasználónév : </label>
+                    <br>
+                    <input type="text" name="username" placeholder="Írja be a felhasználónevet" id="reg" required>
+                    <br><br>
+                    <label>E-mail cím : </label>
+                    <br>
+                    <input type="email" name="email" placeholder="Írja be az e-mail címet" id="reg" required/>
+                    <br><br>
+                    <label>Jelszó : </label>
+                    <br>
+                    <input type="password" name="password" placeholder="Írja be a jelszót" id="reg" required/>
+                    <br><br>
+                    <label>Jelszó megerősítése : </label>
+                    <br>
+                    <input type="password" name="password_2" placeholder="Írja be a jelszót újra" id="reg" required/>
+                    <br><br>
+                    <label>Vezetéknév: </label>
+                    <br>
+                    <input type="text" name="first_name" placeholder="Írja be a Vezetéknevét" id="reg" required/>
+                    <br><br>
+                    <label>Keresztnév: </label>
+                    <br>
+                    <input type="text" name="last_name" placeholder="Írja be a Keresztnevét" id="reg" required/>
+                    <br><br>
+                    <button type="submit" name="adduser" class="adduser">Regisztráció</button>
+                    <br><br>
+                    </div>
+                
+            
+                 </form>
+            </div>       
+        </body>
+    
+
+</html

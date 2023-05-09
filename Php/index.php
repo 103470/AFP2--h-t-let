d3a767621a42bad92234be32051e2f358cd7a543
halfalo <113610878+halfalo@users.noreply.github.com>
2023-05-10 00:02:45 +0200

    index.php query javítása

--- Php/index.php
+++ Php/index.php
@@ -1,67 +1,51 @@
 <?php
 
-require 'connect.php'; 
+require 'connect.php';
+
+
 
 ?>
 <?php
-if(isset($_POST['start'])){
-    $quiz = $_POST['q_name'];
-    header ("location: index.php?page_no=". $next_page);
-}
 
+   
+$quiz = $_GET['start'];
 
-
-    
-    if (isset($_GET['page_no']) && $_GET['page_no']!="") {
-        $page_no = $_GET['page_no'];
-    } 
-        else {
-            $page_no = 1;
-        }
-            
-    $total_records_per_page = 1;
-            
-    $offset = ($page_no-1) * $total_records_per_page;
-    $previous_page = $page_no - 1;
-    $next_page = $page_no + 1;
-    $adjacents = "2";
-            
-    $result_count = mysqli_query($con,"SELECT COUNT(*) As total_records FROM question");
-        $total_records = mysqli_fetch_array($result_count);
-        $total_records = $total_records['total_records'];
-        $total_no_of_pages = ceil($total_records / $total_records_per_page);
-        $second_last = $total_no_of_pages - 1; 
-            
-            
-            
-    $result = mysqli_query($con,"SELECT * FROM question LIMIT $offset, $total_records_per_page");
-            
-            
-
-                
-                
-    if(isset($_POST['next'])){
-        $number = $_POST['number'];
-        $quiz_id = $_POST['quizid'];
-        $quiz_name = $_POST['quizname'];
-        $question_id = $_POST['questionid'];
-        $question = $_POST['question'];
-        $answer = $_POST['answer'];
-            
-        $con->query("INSERT INTO answer (quiz_id, quiz_name, question_id, question, answer) VALUES ('$quiz_id', '$quiz_name', '$question_id', '$question', '$answer')");
-                    
-            
-        if($page_no < $total_no_of_pages) {
-            header ("location: index.php?page_no=". $next_page);
-                    }
+$result = $con->query("SELECT * FROM question WHERE quiz_id = $quiz");
+$result3 = $con->query("SELECT * FROM question_text WHERE quiz_id = $quiz");
+           
+       
+   
+   
+   
+               
+                   
+   if(isset($_POST['save'])){
+           
+    $quiz_id = $_POST['quizid'];
+    $quiz_name = $_POST['quizname'];
+    $question_id = $_POST['questionid'];
+    $question = $_POST['question'];
+    $answer = $_POST['answer'];
+    $question_text = $_POST['questiontext'];
+    $text_answer = $_POST['textanswer'];
+    foreach($answer as $key => $value)
+    {
+        $con->query("INSERT INTO answer (quiz_id, quiz_name, question_id, question, answer) VALUES ('$quiz_id', '$quiz_name', '$question_id', '$question', '$value')");
     }
-        
-            
-            
-            
-            
-    
 
+    $con->query("INSERT INTO answer (quiz_id, quiz_name question_id, question, answer) VALUES ('$quiz_id', '$quiz_name', '$question_id', '$question_text', $text_answer'");
+
+    
+       
+   
+         
+   }
+   
+
+
+
+    
+?>
 <!DOCTYPE html>
 <html>
     <head>

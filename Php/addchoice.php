498c88222c4ccb79da1a6550bc6d369e3729ac14
halfalo <113610878+halfalo@users.noreply.github.com>
2023-05-09 23:38:52 +0200

    addchoice.php hozzáadás gomb javítása
    a gomb nem mentette el a válasszt ez javításra került

--- Php/addchoice.php
+++ Php/addchoice.php
@@ -9,62 +9,77 @@ require 'connect.php'; ?>
         while(mysqli_num_rows($result) > 0){
             ++$qid;
         }
-
-        $qname = $_POST['qname'];
-        //$quiz_id = $con->query("SELECT quiz_id from quiz WHERE quiz_name = ".$qname);
+        
+        $quiz_id = $_POST['qname'];
+        $qname = "";
+        $res = $con->query("SELECT quiz_name FROM quiz WHERE quiz_id = $quiz_id");
+        while($row = mysqli_fetch_assoc($res)){
+            $qname = $row['quiz_name'];
+        }
         $question = $_POST['question'];
-        $choice1 = $_POST['option1'];
-        $choice2 = $_POST['option2'];
-        $choice3 = $_POST['option3'];
-        $choice4 = $_POST['option4'];
+        $choice = array();
+        $choice[1] = $_POST['option1'];
+        $choice[2] = $_POST['option2'];
+        $choice[3] = $_POST['option3'];
+        $choice[4] = $_POST['option4'];
 
-        $con->query("INSERT INTO question (quiz_id, quiz_name, question_id, question, option1, option2, option3, option4) VALUES ('$quiz_id', '$qname', '$qid', '$question', '$choice1', '$choice2','$choice3','$choice4')");
+        $count = count($choice);
+        for($i=1; $i <= $count; $i++){
+
+        $con->query("INSERT INTO q_answers (qid, answer) VALUES ('$qid', '$choice[$i]')");
+        }
+
+        $con->query("INSERT INTO question (quiz_id, quiz_name, question_id, question) VALUES ('$quiz_id', '$qname', '$qid', '$question')");
     }
 
     $quizname = $con->query("SELECT * FROM quiz");
+    
 
+    
 
-
-
-    $query = "SELECT * FROM question";
-    $questions = mysqli_query($con,$query);
-
-
+    
+	
+		
 
 
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Kérdés hozzáadása</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="index.css">
     </head>
     <body>
-    <header>
-        <ul>
-            <li><a href="addchoice.php">Kérdés hozzáadása</a></li>
-            <li><a href="addqtext.php">Predikció hozzáadása</a></li>
-            <li><a href="logout.php">Kijelentkezés</a></li>
-        </ul>
-    </header>
+    <nav>
+       <div class="container">
+
+       <div class="menu">
+            <a href="addquiz.php">Quiz hozzáadása</a>
+            <a href="editquiz.php">Quiz szerkesztése</a>
+            <a href="addchoice.php">Feleletválasztós kérdés hozzáadása</a>
+            <a href="addqtext.php">Kifejtendő kérdés hozzáadása</a>
+            <a href="editq.php">Kérdés szerkesztése</a>
+            <a href="adduser.php">Felhasználó hozzáadása</a>
+            <a href="logout.php">Kijelentkezés</a>
+        </div>
+
+       </div>
+    </nav>
         <main>
-            <div class="nagytest">
+            <div class="box">
             <h2 class="choice">Kérdés hozzáadása</h2>
             <form method="POST" action="addchoice.php">
-                <div class="test">
+                <div class="inputBox">
                 <br>
-                    <select name="qname" value="<?php echo $row['quiz_name']; ?>">
+                    <select name="qname">
                         <option>Quiz címe</option>
-                        
                         <?php while($row = mysqli_fetch_assoc($quizname)){ ?>
-                            <option value="<?php echo $row['quiz_name']; ?>" name="qname"><?php echo $row['quiz_name']; ?></option>
+                            <option value="<?php echo $row['quiz_id']; ?>" ><?php echo $row['quiz_name']; ?></option>
                         <?php } ?>
                     </select>
             
                 </br>
-                <br>
-                    <input type="hidden" name="number" class="szam" value="<?php echo $next;  ?>">
-                <br>
                 <br>
                     <label>Kérdés :</label>
                     <input type="text" name="question" class="textbox">

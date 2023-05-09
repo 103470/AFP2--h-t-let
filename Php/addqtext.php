ac587f8e4ea0ca3e46e18fa89651ceb335f0ff78
Saviur <113610538+103470@users.noreply.github.com>
2023-05-10 00:15:22 +0200

    addqtext.php - html kódrész feltöltése

--- Php/addqtext.php
+++ Php/addqtext.php
@@ -23,3 +23,52 @@ $quizname = $con->query("SELECT * FROM quiz");
 		
     
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Kérdőív szerkesztése</title>
+        <link rel="stylesheet" type="text/css" href="index.css">
+    </head>
+    <body>
+    <nav>
+       <div class="container">
+
+       <div class="menu">
+            <a href="addquiz.php">Quiz hozzáadása</a>
+            <a href="editquiz.php">Quiz szerkesztése</a>
+            <a href="addchoice.php">Feleletválasztós kérdés hozzáadása</a>
+            <a href="addqtext.php">Kifejtendő kérdés hozzáadása</a>
+            <a href="editq.php">Kérdés szerkesztése</a>
+            <a href="adduser.php">Felhasználó hozzáadása</a>
+            <a href="logout.php">Kijelentkezés</a>
+        </div>
+
+       </div>
+    </nav>
+    <main>
+    <div class="box">
+    <form method="POST" action="addqtext.php">
+        <div class="inputBox">
+            <br>
+                <select name="quizid">
+                    <option>Quiz címe</option>
+                    <?php while($row = mysqli_fetch_assoc($quizname)){ ?>
+                        <option value="<?php echo $row['quiz_id']; ?>" ><?php echo $row['quiz_name']; ?></option>
+                    <?php } ?>
+                </select>
+            
+            </br>
+            <br>
+                <label>Kérdés:</label>
+                <input type="text" name="question">
+            <br><br>    
+
+            <button name="addqt" value="addqt">Hozzáadás</button>
+        </div>
+    </form>
+    </div>
+    
+    </main>
+    </body>
+</html>

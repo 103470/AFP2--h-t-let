c607be96c7ab0dab921f8ef07cbcde0562a9bfa3
halfalo <113610878+halfalo@users.noreply.github.com>
2023-05-17 08:43:50 +0200

    quizeditp.php session megírása

--- Php/quizeditp.php
+++ Php/quizeditp.php
@@ -1,6 +1,12 @@
 <?php 
 
-require 'connect.php'; ?>
+require 'connect.php'; 
+
+if(!isset($_SESSION['username'])){
+    header("location: login.php");
+}
+
+?>
 
 <?php
 if (isset($_GET['page_no']) && $_GET['page_no']!="") {
